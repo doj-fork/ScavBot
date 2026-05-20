98bc85c57063d7c81233baf1f2972614b7ea35a7
doj-fork <doj10@uci.edu>
2026-05-19 23:59:30 -0700

    updated upgrades screen sfx + intermission scene

--- Overlays/upgrades.gd
+++ Overlays/upgrades.gd
@@ -1,22 +1,38 @@
 extends CanvasLayer
 
+@onready var hover_sfx: AudioStreamPlayer2D = $hoverSFX
+@onready var click_sfx: AudioStreamPlayer2D = $clickSFX
+
 func _ready():
 	visible = false
 
 
+func _on_button_hover() -> void:
+	if hover_sfx.stream != null:
+		hover_sfx.play()
+
+
+func _on_button_click() -> void:
+	if click_sfx.stream != null:
+		click_sfx.play()
+
+
 func _on_health_pressed() -> void:
+	_on_button_click()
 	Stats.health += 25
 	Stats.healthMult += 5
 	visible = false
 	Global.hudActive = true
 
 func _on_speed_pressed() -> void:
+	_on_button_click()
 	Stats.speed += 25
 	Stats.speedMult += 25
 	visible = false
 	Global.hudActive = true
 
 func _on_damage_pressed() -> void:
+	_on_button_click()
 	Stats.damageMult += 3
 	visible = false
 	Global.hudActive = true
